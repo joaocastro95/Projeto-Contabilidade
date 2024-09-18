fdbcde9282b4efd27434544bb5e43535ba8b3c27
lab3.3 <lab33@fatec.com>
2024-09-18 10:58:07 -0300

    aplicacao de handlebars

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 import express from 'express'
+import { sequilize } from './db/connection.js';
+import exphbs from 'express-handlebars'
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const db = sequilize; 
 const app = express();
 const PORT = 3000;
+
+//bodyparser
+app.use(bodyParser.urlencoded({extended: false}));
+
+//handle bars
+app.set('views',path.join(__dirname,'views'))
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}) );
+app.set('view engine', 'handlebars');
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//db connection
+db.authenticate().then(()=>{
+  console.log('deu bom na conexao')
+}).catch(err => {
+  console.log("ocorreu um erro na conexao", err)
+});
 
 
 app.listen(PORT,() => {
@@ -9,6 +37,22 @@ app.listen(PORT,() => {
 })
 
 app.get('/', (req,res) => {
-    res.send("ta dando otimo")
+    res.render('index');
+})
+
+app.get('/cadastro',(req,res)=>{
+  res.render('cadastro')
+})
+
+app.get('/home',(req,res)=>{
+  res.render('home')
+})
+
+app.get('/perfil',(req,res)=>{
+  res.render('perfil')
+})
+
+app.get('/login',(req,res)=>{
+  res.render('login')
 })
 
